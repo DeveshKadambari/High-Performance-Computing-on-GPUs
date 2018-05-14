e580243cfcc15cd21dc4769b49b247f6a85c0bd8
Devesh Kadambari <Saiidevesh_kadambari@student.uml.edu>
2018-05-14 19:03:43 -0400

    Running

--- Project/CNN.cpp
+++ Project/CNN.cpp
@@ -14,51 +14,42 @@ using namespace std;
 using namespace cv;
 
 /*functions*/
-void convolution(int org_n,int *org,int *out,int fil_f,int *fil,int pad,int str,int n_fil);
+void convolution(int org_n,double *org,double *out,int fil_f,int *fil,int pad,int str,int n_fil);
 void filter(int *size,int *fil,int n_fil);
 //void fully_connected(int out_n,int *in,int *out,int fil_f,int *fil,int *y);
-void softmax(int *in,double	 *out);
+void softmax(double *in,double	 *out);
 void loss_function(double *predicted,int *truth,double *MSE);
-void back_propagation(int *b,double *out,int *target,int max_fil,int fil_f,int *fil_out,int *fil_1,int *fil_0,int *out_1,int *out_2);
+void back_propagation(double *b,double *out,int *target,int max_fil,int fil_f,int *fil_out,int *fil_1,int *fil_0,double *out_1,double *out_2);
 /*main thread*/
 int main( int argc, const char** argv )
 {
-//cout<<"hi_beginning"<<endl;
 Mat img;
-int *org_o,*out_1,*fil__f,*out;
+int *org_o,*fil__f;
+double *out_1,*out;
 img=imread(argv[1], CV_LOAD_IMAGE_COLOR);
-
+cvtColor(img, img, CV_BGR2GRAY);
 unsigned char *org = (unsigned char*)(img.data);		// getting the image BGR data into char array(0->225) with 3 channels 
-//cout<<"hi_before_separating_b_g_r"<<endl;
-
-int i,k,*b;
-
+int i,k;
+double *b;
 int green=img.rows*img.cols;
 int red= green*2;
-int *fil_0,*fil_1,*fil_out,fil_f=5;
-//cout<<"hi_1"<<endl;
+int *fil_0,*fil_1,*fil_out,fil_f=32;
 fil_0=(int*) malloc(sizeof(int)*fil_f*fil_f*5);
 fil_1=(int*) malloc(sizeof(int)*fil_f*fil_f*5);
-//cout<<"hi_2"<<endl;
 int koo=img.rows-fil_f+1;
-out_1=(int*) malloc(sizeof(int)*koo*koo*5);
-int *out_2;
-//cout<<"hi_3"<<endl;
+out_1=(double*) malloc(sizeof(double)*koo*koo*5);
+double *out_2;
 int foo= koo-fil_f+1;
-out_2=(int*) malloc(sizeof(int)*foo*foo*5);
+out_2=(double*) malloc(sizeof(double)*foo*foo*5);
 fil_out=(int*) malloc(sizeof(int)*fil_f*fil_f*5);
-//cout<<"hi_4"<<endl;
 int poo=foo-fil_f;
-out=(int*) malloc(sizeof(int)*poo*poo*4);
+out=(double*) malloc(sizeof(double)*poo*poo*4);
 double *d_out;
 d_out=(double*) malloc(sizeof(double)*poo*poo*4);
 double *MSE;
-//cout<<"hi_5"<<endl;
 MSE=(double*) malloc(sizeof(double)*1);
 int *truth;
-
 truth=(int*) malloc(sizeof(int)*4);
-//cout<<"hi_6"<<endl;
 for(int i=0;i<4;i++)
 {
 if(i==2){
@@ -68,18 +59,12 @@ else
 truth[i]=0;
 }
 /*input layer 1*/
-int a=img.rows*img.cols*3;
-b=(int*) malloc(sizeof(int)*a);
-//cout<<"allocation"<<endl;
-
+int a=img.rows*img.cols;
+b=(double*) malloc(sizeof(double)*a);
 for(int i = 0;i < img.rows ;i++){
         for(int j = 0;j < img.cols ;j++){
-            //cout<<"hi_11"<<endl;
-	    b[img.cols * i + j] = org[img.cols * i + j ] ;			//Load mxmx3 matrix into the integer pointer with pixel values
-		//cout<<"hi_13"<<endl;
-            b[img.cols * i + j+green]=  org[img.cols * i + j + 1];			//After that sending it to convolution.
-		//cout<<"hi_14"<<endl;
-            b[img.cols * i + j+red] = org[img.cols * i + j + 2];
+                b[img.cols * i + j] = org[img.cols * i + j ] ;			//Load mxmx3 matrix into the integer pointer with pixel values
+		
         }
     }
 int max_fil=fil_f*fil_f*5;
@@ -114,24 +99,24 @@ filter(&fil_f,fil_out,4);
 do{
 //Hidden 1 layer 1
 convolution(img.rows, b,out_1,fil_f,fil_0,0,1,5);
-//cout<<"conv_1 done"<<endl;
+cout<<"conv_1 done"<<endl;
 
 //Hidden 2 layer 2
 convolution(koo, out_1,out_2,fil_f,fil_1,0,1,5);
-//cout<<"conv_2 done"<<endl;
+cout<<"conv_2 done"<<endl;
 
 //output layer
-convolution(foo, out_2,out,fil_f,fil_out,0,1,4);
-//cout<<"output layer done"<<endl;
-
+convolution(foo, out_2,out,fil_f,fil_out,0,1,4);			//****working good uptil here****...!!
+cout<<"output layer done"<<endl;
+								
 softmax(out,d_out);
 loss_function(d_out,truth,MSE);						//Truth not declared
-//cout<<"loss function done"<<endl;
+cout<<"loss function done"<<endl;
 //for(int i=0;i<4;i++)
 //{
 //cout<<"out["<<i<<"]="<<out[i];
 //}
-//cout<<"MSE"<<*MSE<<endl;
+cout<<"MSE"<<*MSE<<endl;
 back_propagation(b,d_out,truth,max_fil,fil_f,fil_out,fil_1,fil_0,out_1,out_2);//back_propagation(int *b,int *out,int *target,int max_fil,int fil_f,int *fil_out,int *fil_1,int *fil_0,int *out_1,int *out_2)
 //cout<<"back propagation done"<<endl;
 ab=d_out[2] ;
@@ -139,7 +124,7 @@ ba=truth[2];
 cout<<"ab = "<<ab<<endl;
 cout<<"ba ="<<ba<<endl;
 }
-while(ab != 0.5*ba);
+while((ab != 0.5*ba)||(ab >= 0.5*ba));
 
 waitKey(0);
 
@@ -152,7 +137,7 @@ return 0;
 
 /*First layer of CNN has Convlution,striding and activation function(ReLU)*/
 
-void convolution(int org_n,int *org,int *out,int fil_f,int *fil,int pad,int str,int n_fil) //convolution(img.rows, *b,*out1,fil_f,*fil_0,0,0);
+void convolution(int org_n,double *org,double *out,int fil_f,int *fil,int pad,int str,int n_fil) //convolution(img.rows, *b,*out1,fil_f,*fil_0,0,0);
 {
 
 int incstr=str,o_row;
@@ -186,8 +171,11 @@ for (int i = 0; i < f_row; i++)
         }
   }
 
-out[l*o_col+k+ln]++;									//adding bias
-out[l*o_col+k+ln]=max(0,out[l*o_col+k+ln]);							//Using Acivation function
+out[l*o_col+k+ln]++;
+if(out[l*o_col+k+ln]<0)
+{									//adding bias
+out[l*o_col+k+ln]=0;
+}								//Using Acivation function
 //if((org_col+fil_f)%org_n)>=0){org_col=org_col+fil_f;}
 org_col+=str;										//striding
 //cout<<"out["<<l<<"*"<<o_col<<"+"<<k<<"+"<<ln<<"]"<<out[l*o_col+k+ln]<<endl;
@@ -220,6 +208,7 @@ for(int n=0;n<n_fil;n++)
 p+=fil_f*fil_f;
 }
 }
+
 /*full connected layer*/
 /*
 void fully_connected(int out_n,int *in,int *out,int fil_f,int *fil,int *y)
@@ -234,9 +223,10 @@ for(int k=0;k<out_size;k++)
 
 }
 */
+
 /*softmax layer*/
 
-void softmax(int *in,double *out)
+void softmax(double *in,double *out)
 {
 double *t,sum;
 t=(double*) malloc(sizeof(double)*4*4);
@@ -268,10 +258,12 @@ MSE+=1/4*joo;
 
 }
 /*back propagation*/
-void back_propagation(int *b,double *out,int *target,int max_fil,int fil_f,int *fil_out,int *fil_1,int *fil_0,int *out_1,int *out_2)
+void back_propagation(double *b,double *out,int *target,int max_fil,int fil_f,int *fil_out,int *fil_1,int *fil_0,double *out_1,double *out_2)
 {
+
 int eta=0.01,p=0;
 /*This is for last filter fil_out */
+
 for(int n=0;n<max_fil;n++)
 {
 int dE_dw=(out[n]-target[n])*(out[n]*(1-out[n]))*out_2[n];
@@ -283,6 +275,7 @@ int dE_dw=(out[n]-target[n])*(out[n]*(1-out[n]))*out_2[n];
 }
 p=fil_f*fil_f;
 }
+
 /*This is for last filter fil_1 dEtotal/dwh2 = dE_total/dout_h2 *dout_h2/dnet_h2 * dnet_h2/dw_h2 */
 int dE_dw,dE_dw_0;
 for(int n=0;n<max_fil;n++)
@@ -299,10 +292,11 @@ int dEtotal_dout_h2=dE_1_dout_h2+dE_2_dout_h2+dE_3_dout_h2+dE_4_dout_h2;
 	   
         }
 }
-/*This is for last filter fil_0 dEtotal/dwh2 = dE_total/dout_h1 *dout_h1/dnet_h1 * dnet_h1/dw_h1 */
-int dEtotal_dout_1=dEtotal_dout_h2*1*fil_1[n];
-    
 
+/*This is for last filter fil_0 dEtotal/dwh2 = dE_total/dout_h1 *dout_h1/dnet_h1 * dnet_h1/dw_h1 */
+
+int dEtotal_dout_1=dEtotal_dout_h2*1*fil_1[n];
+ 
 for (int i = 0; i <fil_f; ++i) {
         for (int j = 0; j < fil_f; ++j) {
 		dE_dw_0 = dEtotal_dout_1*1*b[i * fil_f + j+p];
@@ -311,11 +305,7 @@ for (int i = 0; i <fil_f; ++i) {
         }
 }
 
-
 }
-
-
-
-
+cout<<"before_for2 backprop"<<endl;
 
 }
